220424da4139ff99cb5255cc547c07ca7566bca6
morga-dev <d.morga.dev@gmail.com>
2025-06-25 00:35:22 -0600

    Arreglo en la petición POST del front

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
     e.preventDefault();
     setMsg("");
     try {
-      const res = await fetch("https://bearstravel-api-production.up.railway.app", {
+      const res = await fetch("https://bearstravel-api-production.up.railway.app/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
